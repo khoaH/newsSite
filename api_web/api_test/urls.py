0cfdf90112439bb6f2c6aaf21a91f096821e13c2
khoaH <44110166+khoaH@users.noreply.github.com>
2021-05-12 09:25:37 +0700

    Second large update
    -map admin DONE
    -register DONE But not key press catching
    -log out DONE
    -edit/delete post DONE
    -> Post managing navigation DONE
    TODO:
    -add/edit/delete category
    - add/edit/delete account
    - approve post

--- api_web/api_test/urls.py
+++ api_web/api_test/urls.py
@@ -8,9 +8,14 @@ urlpatterns = [
         path('contact.html', views.contact, name='contact'),
         path('category/<int:id_category>.html', views.category, name='category'),
         path('login.html', views.login, name='login'),
+        path('logout.html', views.logout, name='logout'),
+        path('admin.html', views.admin, name='admin'),
+        path('pages-post.html', views.newsController, name='newsController'),
         path('page.html', views.page, name='page'),
         path('post/<int:post_id>.html', views.post, name='post'),
         path('register.html', views.register, name='register'),
         path('search.html', views.search, name='search'),
-        path('post/add.html',views.addPost, name='addPost')        
+        path('addPost.html',views.addPost, name='addPost'),
+        path('editPost/<int:post_id>.html', views.editPost, name='editPost'),
+        path('deletePost/<int:post_id>.html', views.deletePost, name='deletePost')        
 ]
